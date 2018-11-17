e35c4a57a25f4fb355648d3bfb1a42fa3eccb790
amnesiaice <jxw9391@qq.com>
2018-11-17 19:44:42 +0800

    fix the light normal dot calculate

--- PythonRendererCode/graphic_manager.py
+++ PythonRendererCode/graphic_manager.py
@@ -6,16 +6,31 @@ import PythonRendererCode.resource_manager as resource_manager
 l_pi = np.pi
 
 
-def make_sphere(p_img,p_radius):
+def make_sphere(p_img, p_radius):
     l_height = len(p_img)
     l_width = len(p_img[0])
     l_center_x = int(l_width/2)
     l_center_y = int(l_height/2)
+    l_item_color = resource_manager.item_color
+    l_radius = resource_manager.radius
+    l_light_direct = resource_manager.light_direct
+    l_back_ground_color = resource_manager.back_ground_color
     for j in range(0, l_width-1):
         for i in range(0, l_height-1):
             if np.square(j-l_center_x)+np.square(i-l_center_y) < np.square(p_radius):
-                p_img[i][j] = resource_manager.item_color
+                l_item_light = __sphere_norm_light(j-l_center_x, i-l_center_y, l_radius, l_light_direct)
+                p_img[i][j] = np.multiply(l_item_color, l_item_light)
+                p_img[i][j][3] = 1
             else:
-                p_img[i][j] = resource_manager.back_ground_color
+                p_img[i][j] = l_back_ground_color
     return p_img
 
+
+def __sphere_norm_light(p_x, p_y, p_radius, p_light):
+    l_norm = (p_x, p_y, np.sqrt(np.square(p_radius)-np.square(p_x)-np.square(p_y)))
+    out = np.dot(p_light, l_norm)/__vector_length(p_light)/__vector_length(l_norm)*0.5+0.5
+    return out
+
+
+def __vector_length(p_vector):
+    return np.sqrt(np.dot(p_vector, p_vector))
--- PythonRendererCode/resource_manager.py
+++ PythonRendererCode/resource_manager.py
@@ -1,9 +1,9 @@
 import matplotlib.image as mpimg
 
 # parameter used in this project
-light_direct = (1, 1, 1)
+light_direct = (1, -1, 1)
 radius = 100
-item_color = (0, 0, 1, 1)
+item_color = (0.8, 0.8, 0.8, 1)
 back_ground_color = (0.7, 0.3, 0.3, 1)
 
 # 600x600 pix target
