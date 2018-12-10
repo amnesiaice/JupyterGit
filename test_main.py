2adc7ec12d2e812a5970268c878bfff1747838b5
amnesiaice <jxw9391@qq.com>
2018-12-11 00:28:13 +0800

    implement a simple canvas to init a blank screen with pure color,. and reconstruct the test_main.py to make the test easier to understand

--- test_main.py
+++ test_main.py
@@ -1,5 +1,19 @@
-import PythonRendererCode.feature_manger as feamgr
+def test_case(p_case):
+    if p_case == 0:
+        import PythonRendererCode.feature_manger as feamgr
+        print('display sphere')
+        feamgr.display_sphere()
+    elif p_case == 1:
+        import PythonRendererCode.resource_manager as rcsmgr
+        rcsmgr.current_canvas.set_canvas(10, 5, 'red')
+        rcsmgr.current_canvas.draw_canvas()
+    elif p_case == 2:
+        import PythonRendererCode.resource_manager as rcsmgr
+        rcsmgr.current_canvas.set_canvas(800, 600, 'light blue')
+        rcsmgr.current_canvas.draw_canvas()
 
-# test feature
-print('display sphere')
-feamgr.display_sphere()
+
+# 0 |   display_sphere()  (this test will be remove after the feature_manager is disused)
+# 1 |   canvas test for default red color
+# 2 |   canvas test for paint something
+test_case(1)
